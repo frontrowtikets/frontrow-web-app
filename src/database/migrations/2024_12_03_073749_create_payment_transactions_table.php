6f1ec162f84b7cc9506705e9e70fa4512a456023
Tumuhimbise Godwin <65310616+ushergodwin@users.noreply.github.com>
2024-12-03 14:47:49 +0300

    feat: transaction table migration

---
+++ src/database/migrations/2024_12_03_073749_create_payment_transactions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payment_transactions', function (Blueprint $table) {
+            $table->uuid('id')->primary()->default(DB::raw('gen_random_uuid()'));
+            $table->string('txn_ref', 100);
+            $table->string('mfscode', 100);
+            $table->string('txn_type', 50)->default('ticket_purchase');
+            $table->string('txn_channel', 50)->default('web');
+            $table->string('txn_status', 20)->default('pending');
+            $table->bigInteger('amount');
+            $table->string('currency', 5)->default("UGX");
+            $table->string('reason');
+            $table->string('phone_number', 14);
+            $table->foreignUuid('user_id')->constrained();
+            $table->string('txn_hash', 64);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payment_transactions');
+    }
+};
