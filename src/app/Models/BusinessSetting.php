35952b4f798894a60c68f79484f3cb6da19ff8d8
Tumuhimbise Godwin <65310616+ushergodwin@users.noreply.github.com>
2025-03-22 11:59:43 +0300

    business configurations

---
+++ src/app/Models/BusinessSetting.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class BusinessSetting extends Model
+{
+    //
+    use SoftDeletes;
+
+    protected $fillable = [
+
+        'service_fee',
+        'share_percentage',
+        'wallet_credit',
+        'shareholder_wallet_id',
+    ];
+
+    protected $casts = [
+        'service_fee' => 'integer',
+        'share_percentage' => 'integer',
+        'wallet_credit' => 'integer',
+        'shareholder_wallet_id' => 'integer',
+    ];
+
+    public function shareholderWallet()
+    {
+        return $this->belongsTo(UserWallet::class, 'shareholder_wallet_id');
+    }
+}
---
+++ src/database/migrations/2025_03_22_081711_create_business_settings_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('business_settings', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('service_fee')->default(0);
+            $table->bigInteger('share_percentage')->default(0);
+            $table->bigInteger('wallet_credit')->default(0);
+            $table->bigInteger('shareholder_wallet_id')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('business_settings');
+    }
+};
