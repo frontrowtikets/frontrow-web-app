c201eda51f7615e3631ae3f72fed050134e1cfa5
Tumuhimbise Godwin <65310616+ushergodwin@users.noreply.github.com>
2025-03-22 11:59:23 +0300

    noindex for dev server

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -4,7 +4,14 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'FrontRow Tikets') }}</title>
+
+        <!-- Favicon -->
+        <link rel="icon" type="image/png" href="/small.png">
+        <link rel="apple-touch-icon" href="/small.png">
+        @if (request()->getHost() === 'frontrow.cinemaug.com')
+            <meta name="robots" content="noindex, nofollow">
+        @endif
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
